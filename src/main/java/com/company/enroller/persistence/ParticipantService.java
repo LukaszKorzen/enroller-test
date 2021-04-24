a2a91fe6f096c8a345f1c34fae1ea80fdbf1bbeb
Lukasz <korzen1984@gmail.com>
2021-04-24 15:12:43 +0200

    changes

---
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -0,0 +1,51 @@
+package com.company.enroller.persistence;
+
+import java.util.Collection;
+
+import javax.transaction.Transaction;
+
+import org.hibernate.classic.Session;
+import org.springframework.stereotype.Component;
+
+import com.company.enroller.model.Participant;
+
+@Component("participantService")
+public class ParticipantService {
+	
+	Session session;
+	
+
+
+	public ParticipantService() {
+		session = DatabaseConnector.getInstance().getSession();
+	}
+
+	public Collection<Participant> getAll() {
+		return session.createCriteria(Participant.class).list();
+	}
+    public Participant findByLogin(String login) {
+    	return (Participant) session.get(Participant.class,  login);
+    	
+    }
+
+	public void add(Participant participant) {
+		Transaction transaction = session.beginTransaction();
+		session.save(participant);
+		transaction.commit();
+	}	
+		public void delete(Participant participant) {
+			Transaction transaction = session.beginTransaction();
+			session.delete(participant);
+			transaction.commit();
+			
+			
+		}
+			
+		public void update(Participant participant) {
+			Transaction transaction = session.beginTransaction();
+			session.merge(participant);
+			transaction.commit();
+			
+		
+	}
+
